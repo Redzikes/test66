5eff1753329b7af03be9d885a34c626fe171d02d
Redzikesxd <tomekreda16@gmail.com>
2018-07-13 19:48:48 +0200

    asda

---
+++ src/main/java/pl/xentivo/app2work/quarz/Application.java
@@ -0,0 +1,12 @@
+package pl.xentivo.app2work.quarz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/pl/xentivo/app2work/quarz/job/EmailJob.java
@@ -0,0 +1,22 @@
+package pl.xentivo.app2work.quarz.job;
+
+import pl.xentivo.app2work.quarz.service.EmailService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by gkatzioura on 6/6/16.
+ */
+public class EmailJob implements Job {
+
+    @Autowired
+    private EmailService cronService;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        cronService.sendSpam();
+    }
+}
